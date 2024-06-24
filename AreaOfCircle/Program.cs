cb95aba9819ddac95c0a0279436fc923d8d43a06
Melany Willis <melexiw@gmail.com>
2024-06-24 16:00:45 -0500

    Completed road trip

--- AreaOfCircle/Program.cs
+++ AreaOfCircle/Program.cs
@@ -1,7 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Please enter a radius: ");
 double radius = double.Parse(Console.ReadLine());
-double area = Math.PI * radius * radius;
+double pi = 3.14;
+double area = pi * radius * radius;
 Console.WriteLine("The area of the circle is " + area);
-Console.WriteLine("The circumference of the circle is " + 2 * Math.PI * radius);
+Console.WriteLine("The circumference of the circle is " + 2 * pi * radius);
 Console.WriteLine("The diameter of the circle is " + 2 * radius);
+
+Console.WriteLine("What is your MPG? ");
+double mpg = double.Parse(Console.ReadLine());
+double gallons = area / mpg;
+Console.WriteLine("You will need " + gallons + " gallons of gas to travel around the circle");
